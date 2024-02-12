8c56623fec955b2ab68c33d48a4f55fb15542bfb
Powdee123 <66191086+Powdee123@users.noreply.github.com>
2024-02-11 16:01:59 -0800

    fixed meta data

--- app/(developments)/developments/layout.tsx
+++ app/(developments)/developments/layout.tsx
@@ -1,4 +1,11 @@
 import Navbar from "./_components/navbar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Xamaa Developments",
+  description:
+    "Xamaa Development provide Web development, design, and SEO services in Victoria, BC. We have collection of works you can view. You can request for a FREE UI design for your business.",
+};
 
 export default function DevelopmentLayout({
   children,
