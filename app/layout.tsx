500570967687ba547ceb6b1bf61968bf154a64e4
Powdee123 <66191086+Powdee123@users.noreply.github.com>
2024-04-29 18:18:31 -0700

    changed og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Bricolage_Grotesque, ABeeZee } from "next/font/google";
+import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
 
 const bricolage = Bricolage_Grotesque({ subsets: ["latin"] });
@@ -8,6 +8,9 @@ export const metadata: Metadata = {
   title: "Xamaa Developments",
   description:
     "Xamaa Developments provide Web development, design, and SEO services in Victoria, BC. We have collection of works you can view. You can request for a FREE UI design for your business.",
+  openGraph: {
+    images: ["@/public/openGraph/Flyer.png"],
+  },
 };
 
 export default function RootLayout({
