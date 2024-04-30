445bb401c3a0757b703d661c88ca7add76e8deb0
Powdee123 <66191086+Powdee123@users.noreply.github.com>
2024-04-29 18:27:07 -0700

    fixed og image

--- app/(developments)/developments/layout.tsx
+++ app/(developments)/developments/layout.tsx
@@ -5,9 +5,6 @@ export const metadata: Metadata = {
   title: "Xamaa Developments",
   description:
     "Xamaa Development provide Web development, design, and SEO services in Victoria, BC. We have collection of works you can view. You can request for a FREE UI design for your business.",
-  openGraph: {
-    images: ["@/public/openGraph/Flyer.png"],
-  },
 };
 
 export default function DevelopmentLayout({
